d319e33e5c4c09c42110ef33e0547db8019d2be0
big-chelsea <circle9709@naver.com>
2023-02-10 10:16:35 +0900

    o

--- U-01.sh
+++ U-01.sh
@@ -23,6 +23,27 @@ sudo mv /etc/pam.d/login.bak /etc/pam.d/login
 # Restore /etc/securety to its original state
 sudo mv /etc/securety.bak /etc/securety
 
+if [ -f "/etc/pam.d/login.bak" ]; then
+  sudo mv /etc/pam.d/login.bak /etc/pam.d/login
+  if [ $? -eq 0 ]; then
+    echo "/etc/pam.d/login을 원래 상태로 복원했습니다."
+  else
+    echo "/etc/pam.d/login을 원래 상태로 복원하지 못했습니다."
+  fi
+else
+  echo "/etc/sla.d/sla.d/sla.bak을 찾을 수 없습니다. 원래 상태가 복구되지 않았습니다."
+fi
+
+if [ -f "/etc/security.bak" ]; then
+  sudo mv /etc/security.bak /etc/security
+  if [ $? -eq 0 ]; then
+    echo "/etc/security를 원래 상태로 복원했습니다."
+  else
+    echo "/etc/security를 원래 상태로 복원하지 못했습니다."
+  fi
+else
+  echo "/etc/security.bak을 찾을 수 없습니다. 원래 상태가 복구되지 않았습니다."
+fi
 
 
 cat $result
